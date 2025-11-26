91e861d6576fff61d735e43e25621c6ebfb8fb9f
Haman-to-thapa <hamanthapa0000@gmail.com>
2025-11-26 16:08:23 +0530

    04. authenecation

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,14 @@
+import { Register } from '@/features/auth/components/register_form'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Register />
+    </div>
+  )
+}
+
+export default page
